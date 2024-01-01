cf3e8ae9b26cfd565e68d4ba6a00b1096eccc729
uttam kamar <kamaruttam@gmail.com>
2024-01-01 09:00:49 +0530

    chat-app

---
+++ src/pages/dashboard/GeneralApp.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Chats from "./Chats";
+import { Box, Stack } from "@mui/material";
+import Conversation from "../../components/Conversation";
+
+const GeneralApp = () => {
+  return (
+    <Stack direction={"row"} sx={{ width: "100%" }}>
+      {/* Chats */}
+      <Chats />
+
+      <Box
+        sx={{
+          height: "100%",
+          width: "calc(100vw - 420px)",
+          backgroundColor: "#fff",
+        }}
+      >
+        {/* convertation */}
+        <Conversation/>
+      </Box>
+    </Stack>
+  );
+};
+
+export default GeneralApp;
